ec98b8efb55c5d53f7fd94e21ca88ad05f6d8980
Leonid Lovsky <leonidlovsky@Leonids-Laptop.local>
2024-02-28 21:02:38 +0200

    Initial commit

---
+++ api_yamdb/api/mixins.py
@@ -0,0 +1,17 @@
+from rest_framework import mixins, viewsets
+from rest_framework.filters import SearchFilter
+from api.permissions import IsAdminOrReadOnly
+from .pagination import PagePagination
+
+
+class MyCustomBaseViewSet(
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    permission_classes = (IsAdminOrReadOnly,)
+    pagination_class = PagePagination
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
---
+++ api_yamdb/api/views.py
@@ -0,0 +1,126 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Avg
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,
+    IsAuthenticated
+)
+from rest_framework.response import Response
+
+from api.permissions import (
+    IsAdminOrReadOnly,
+    IsAdmin,
+    IsAuthorModeratorAdminOrReadOnly
+)
+from reviews.models import Title, Review, Comments, Category, Genre
+from .serializers import (
+    ReviewSerializer,
+    CommentsSerializer,
+    CategorySerializer,
+    TitleSerializer,
+    GenreSerializer,
+    UserSerializer,
+)
+from .filters import TitleFilter
+from .pagination import PagePagination
+from .mixins import MyCustomBaseViewSet
+
+User = get_user_model()
+
+
+class CategoryViewSet(MyCustomBaseViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(MyCustomBaseViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    serializer_class = TitleSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    pagination_class = PagePagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+
+    def get_queryset(self):
+        return Title.objects.annotate(
+            rating=Avg('reviews__score')
+        ).order_by('-rating')
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (
+        IsAuthorModeratorAdminOrReadOnly,
+        IsAuthenticatedOrReadOnly
+    )
+    pagination_class = PagePagination
+    filter_backends = set()
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        user = self.request.user
+        serializer.save(author=user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentsSerializer
+    permission_classes = (
+        IsAuthorModeratorAdminOrReadOnly,
+        IsAuthenticatedOrReadOnly
+    )
+    pagination_class = PagePagination
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        title = get_object_or_404(Title, pk=title_id)
+        review = get_object_or_404(Review, pk=review_id, title=title)
+        return Comments.objects.filter(review=review)
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        title = get_object_or_404(Title, pk=title_id)
+        review = get_object_or_404(Review, pk=review_id, title=title)
+        serializer.save(author=self.request.user, review=review)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdmin,)
+    pagination_class = PagePagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
+    lookup_field = 'username'
+
+    @action(
+        methods=['patch', 'get'],
+        detail=False,
+        url_path='me',
+        url_name='me',
+        permission_classes=[IsAuthenticated]
+    )
+    def get_me(self, request):
+        user = self.request.user
+        serializer = self.get_serializer(user)
+        if self.request.method == 'PATCH':
+            serializer = self.get_serializer(
+                user, data=request.data, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        return Response(serializer.data)
